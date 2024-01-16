f3d65763ac6ea008354547d8cf98ac76dacd028a
DavidS17GitHub <108815743+DavidS17GitHub@users.noreply.github.com>
2024-01-15 19:37:53 -0500

    Chapter 4

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -4,8 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
+
 
 def about(request):
     return HttpResponse('<h1>About Page</h1>')
 
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
